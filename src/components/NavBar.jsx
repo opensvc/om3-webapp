d356d2c944c89b4563e22bab0b9422c78c20b254
paulj <pauljouvanceau@gmail.com>
2025-04-09 15:05:37 +0200

    modify logout button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
-import {Link, useNavigate} from "react-router-dom";
-import {AppBar, Toolbar, Typography, Button, Box} from "@mui/material";
-import {FaSignOutAlt} from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const NavBar = () => {
     const navigate = useNavigate();
@@ -13,10 +13,10 @@ const NavBar = () => {
     return (
         <AppBar position="sticky">
             <Toolbar>
-                <Typography variant="h6" sx={{flexGrow: 1}}>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
                     OM3
                 </Typography>
-                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <Button color="inherit" component={Link} to="/nodes">
                         Nodes
                     </Button>
@@ -24,10 +24,16 @@ const NavBar = () => {
                         Objects
                     </Button>
                     <Button
-                        color="error"
-                        startIcon={<FaSignOutAlt/>}
+                        startIcon={<FaSignOutAlt />}
                         onClick={handleLogout}
-                        sx={{boxShadow: 3}}
+                        sx={{
+                            backgroundColor: 'red',
+                            color: 'white',
+                            boxShadow: 3,
+                            '&:hover': {
+                                backgroundColor: 'darkred',
+                            }
+                        }}
                     >
                         Logout
                     </Button>

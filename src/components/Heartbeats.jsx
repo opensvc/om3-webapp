d8980cc8bf2b96fe52402d18db53d6eff9321b41
paulj <pauljouvanceau@gmail.com>
2025-04-22 16:44:22 +0200

    centralize hearts

--- src/components/Heartbeats.jsx
+++ src/components/Heartbeats.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import useEventStore from "../store/useEventStore";
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Tooltip } from "@mui/material";
+import {
+    Box,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    Tooltip,
+} from "@mui/material";
 import HeartIcon from "@mui/icons-material/Favorite";
 import HeartBrokenIcon from "@mui/icons-material/FavoriteBorder";
 import DeviceHubIcon from "@mui/icons-material/DeviceHub";
@@ -10,12 +21,13 @@ import ErrorIcon from "@mui/icons-material/Error";
 const getStreamStatus = (stream) => {
     if (!stream) return { state: "Unknown", icon: <ErrorIcon /> };
 
-    const peer = Object.values(stream.peers || {})[0]; // On récupère le premier peer
+    const peer = Object.values(stream.peers || {})[0];
     const isBeating = peer?.is_beating;
     const state = stream.state;
 
     if (state !== "running") return { state: "Stopped", icon: <HeartBrokenIcon /> };
-    if (isBeating) return { state: "Beating", icon: <HeartIcon color="error" /> };
+    if (isBeating)
+        return { state: "Beating", icon: <HeartIcon color="error" /> };
     return { state: "Idle", icon: <HourglassEmptyIcon color="disabled" /> };
 };
 
@@ -25,7 +37,10 @@ const Heartbeats = () => {
 
     return (
         <Box className="p-6">
-            <Typography variant="h5" className="mb-4 font-semibold text-gray-800 text-center">
+            <Typography
+                variant="h5"
+                className="mb-4 font-semibold text-gray-800 text-center"
+            >
                 🔄 Heartbeat Status Monitor
             </Typography>
 
@@ -34,11 +49,14 @@ const Heartbeats = () => {
                     <TableHead>
                         <TableRow className="bg-gray-100">
                             <TableCell className="font-semibold">Node</TableCell>
-                            <TableCell className="font-semibold">hb#1 RX</TableCell>
-                            <TableCell className="font-semibold">hb#1 TX</TableCell>
+                            <TableCell className="font-semibold" align="center">
+                                hb#1 RX
+                            </TableCell>
+                            <TableCell className="font-semibold" align="center">
+                                hb#1 TX
+                            </TableCell>
                         </TableRow>
                     </TableHead>
-
                     <TableBody>
                         {nodes.map((node) => {
                             const heartbeat = heartbeatStatus[node];
@@ -53,14 +71,32 @@ const Heartbeats = () => {
                             return (
                                 <TableRow key={node} hover>
                                     <TableCell>{node}</TableCell>
-                                    <TableCell>
+                                    <TableCell align="center">
                                         <Tooltip title={rxStatus.state} arrow>
-                                            {rxStatus.icon}
+                                            <Box
+                                                sx={{
+                                                    display: "flex",
+                                                    justifyContent: "center",
+                                                    alignItems: "center",
+                                                    height: "100%",
+                                                }}
+                                            >
+                                                {rxStatus.icon}
+                                            </Box>
                                         </Tooltip>
                                     </TableCell>
-                                    <TableCell>
+                                    <TableCell align="center">
                                         <Tooltip title={txStatus.state} arrow>
-                                            {txStatus.icon}
+                                            <Box
+                                                sx={{
+                                                    display: "flex",
+                                                    justifyContent: "center",
+                                                    alignItems: "center",
+                                                    height: "100%",
+                                                }}
+                                            >
+                                                {txStatus.icon}
+                                            </Box>
                                         </Tooltip>
                                     </TableCell>
                                 </TableRow>

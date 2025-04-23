1783cd325338488200912795687c4b5d0d7ffde6
Cyril Galibern <cyril.galibern@opensvc.com>
2025-04-24 00:03:05 +0200

    Add OIDC Auth Context and update OIDC configuration setup
    Introduce `OidcAuthContext` to manage `UserManager` and its lifecycle via React context.
    Update OIDC configuration to align with updated settings.

--- src/config/oidcConfiguration.js
+++ src/config/oidcConfiguration.js
@@ -4,30 +4,20 @@
 
 const baseUrl = window.location.origin;
 
-console.log("redirect_uri:", baseUrl + "/nodes");
-console.log("silent_redirect_uri:", baseUrl + "/authentication/silent_callback");
-
 const initData = {
-    client_id: "ringfs",
-    redirect_uri: baseUrl + "/authentication/callback",
-    //redirect_uri: baseUrl + "/nodes",
-    response_type: "id_token token",
+    client_id: "om3",
+    redirect_uri: baseUrl + "/auth-callback",
+    silent_redirect_uri: baseUrl + "/auth-callback",
+    response_type: "code",
     scope: "openid profile email",
-    //silent_redirect_uri: baseUrl + "/authentication/silent_callback",
-    silent_redirect_uri: baseUrl + '/authentication/callback',
-    accessTokenExpiringNotificationTime: 60,
-    automaticSilentRenew: true,
-    //loadUserInfo: false,
-    loadUserInfo: true,
-    triggerAuthFlow: true,
+    accessTokenExpiringNotificationTimeInSeconds: 30,
     post_logout_redirect_uri: baseUrl + "/",
-    authority: "",
+    automaticSilentRenew: true,
 };
 
 function oidcConfiguration(authInfo) {
-
     // Check if authInfo and authInfo.openid are defined
-    if (!authInfo || !authInfo.openid || !authInfo.openid.well_known_uri) {
+    if (!authInfo?.openid?.well_known_uri) {
         console.warn("OIDC Configuration fallback: 'authInfo.openid.well_known_uri' is missing. Falling back to default configuration.");
         return initData; // Returns default configuration if OIDC configuration is missing
     }
@@ -46,9 +36,9 @@ function oidcConfiguration(authInfo) {
     // Returns OIDC configuration with custom values
     return {
         ...initData,
-        authority: authInfo.openid.well_known_uri,
-        client_id: authInfo.openid.client_id,
-    };
+        authority: authInfo.openid.well_known_uri.replace(/\/\.well-known\/openid-configuration$/, ''),
+        // client_id: authInfo.openid.client_id,
+    }
 }
 
 export default oidcConfiguration;

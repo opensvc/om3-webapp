defbffb25b57b10b589737b7aa8e18cc10e06778
Cyril Galibern <cyril.galibern@opensvc.com>
2025-04-24 19:03:28 +0200

    Terminate Grid -> Grid2 migration

--- src/components/Cluster.jsx
+++ src/components/Cluster.jsx
@@ -75,7 +75,7 @@ const ClusterOverview = () => {
             </Typography>
 
             <Grid2 container spacing={3}>
-                <Grid2 item xs={12} md={4}>
+                <Grid2  size={{ xs: 12, md: 4 }}>
                     <StatCard
                         title="Nodes"
                         value={nodeCount}
@@ -83,7 +83,7 @@ const ClusterOverview = () => {
                         onClick={() => navigate("/nodes")}
                     />
                 </Grid2>
-                <Grid2 item xs={12} md={4}>
+                <Grid2  size={{ xs: 12, md: 4 }}>
                     <StatCard
                         title="Objects"
                         value={Object.keys(objectStatus).length}
@@ -91,7 +91,7 @@ const ClusterOverview = () => {
                         onClick={() => navigate("/objects")}
                     />
                 </Grid2>
-                <Grid2 item xs={12} md={4}>
+                <Grid2  size={{ xs: 12, md: 4 }}>
                     <StatCard
                         title="Namespaces"
                         value={namespaceCount}
@@ -99,14 +99,14 @@ const ClusterOverview = () => {
                         onClick={() => navigate("/namespaces")}
                     />
                 </Grid2>
-                <Grid2 item xs={12} md={4}>
+                <Grid2  size={{ xs: 12, md: 4 }}>
                     <StatCard
                         title="Heartbeats"
                         value={Object.keys(heartbeatStatus).length}
                         onClick={() => navigate("/heartbeats")}
                     />
                 </Grid2>
-                <Grid2 item xs={12} md={4}>
+                <Grid2  size={{ xs: 12, md: 4 }}>
                     <StatCard
                         title="Pools"
                         value={poolCount}

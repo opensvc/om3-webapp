4bcec8b1d8095f985cca73d1a9b9261fc314701f
Paul Jouvanceau <pauljouvanceau@gmail.com>
2025-10-27 15:33:25 +0100

    Add test file for logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,15 @@
-const isDev = typeof process !== 'undefined' && process.env && process.env.NODE_ENV !== 'production';
+const isDev = (() => {
+    if (typeof process !== 'undefined' && process.env) {
+        return process.env.NODE_ENV !== 'production';
+    }
+    return true;
+})();
 
 const safeSerialize = (arg) => {
     try {
         if (typeof arg === 'string') return arg;
+        if (arg === undefined) return 'undefined';
+        if (arg === null) return 'null';
         return JSON.stringify(arg);
     } catch (e) {
         return String(arg);
@@ -13,27 +20,25 @@ const logger = {
     log: (...args) => {
         if (isDev) console.log(...args);
     },
-    // For test compatibility many tests spy on console.log; map info/debug to console.log as well
     info: (...args) => {
         if (isDev) {
             console.log(...args);
-            if (console.info) console.info(...args);
+            if (typeof console.info !== 'undefined') console.info(...args);
         }
     },
     warn: (...args) => {
         if (isDev) console.warn(...args);
     },
-    // Ensure error also emits to console.log for tests that spy on console.log
     error: (...args) => {
         if (isDev) {
             console.log(...args);
-            if (console.error) console.error(...args);
+            if (typeof console.error !== 'undefined') console.error(...args);
         }
     },
     debug: (...args) => {
         if (isDev) {
             console.log(...args);
-            if (console.debug) console.debug(...args);
+            if (typeof console.debug !== 'undefined') console.debug(...args);
         }
     },
     serialize: safeSerialize,
